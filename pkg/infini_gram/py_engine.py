62d6ae2a016a48eaabf99e3f13a0d03d15f20df3
matthewdicks98 <matthewdicks98@gmail.com>
2026-01-16 17:15:45 +0200

    Switching back from lite

--- pkg/infini_gram/py_engine.py
+++ pkg/infini_gram/py_engine.py
@@ -95,6 +95,7 @@ class Engine:
                 use_ssl=False,
                 aws_access_key_id=os.environ["S3_ACCESS_KEY"],
                 aws_secret_access_key=os.environ["S3_SECRET_KEY"],
+                config=Config(max_pool_connections=30),
             )
         else:
             self.s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
--- pkg/test/s3_test.py
+++ pkg/test/s3_test.py
@@ -26,13 +26,13 @@ def main():
 
     # Timing threading.
     start = time.time()
-    with ThreadPoolExecutor(max_workers=20) as executor:
+    with ThreadPoolExecutor(max_workers=30) as executor:
         futures = (
             executor.submit(
                 engine.find,
                 input_ids=query_ids
             )
-            for query_ids in [[random.randint(0, 65535) for _ in range(5)] for _ in range(20)]
+            for query_ids in [[random.randint(0, 65535) for _ in range(5)] for _ in range(30)]
         )
 
         for future in as_completed(futures):
