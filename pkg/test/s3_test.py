ec0ae4c6f77f0f21532c595701632f9b5046dcf3
matthewdicks98 <matthewdicks98@gmail.com>
2026-01-16 16:51:20 +0200

    Testing high concurrency

--- pkg/test/s3_test.py
+++ pkg/test/s3_test.py
@@ -26,13 +26,13 @@ def main():
 
     # Timing threading.
     start = time.time()
-    with ThreadPoolExecutor(max_workers=200) as executor:
+    with ThreadPoolExecutor(max_workers=50) as executor:
         futures = (
             executor.submit(
                 engine.find,
                 input_ids=query_ids
             )
-            for query_ids in [[random.randint(0, 65535) for _ in range(10)] for _ in range(200)]
+            for query_ids in [[random.randint(0, 65535) for _ in range(10)] for _ in range(50)]
         )
 
         for future in as_completed(futures):
