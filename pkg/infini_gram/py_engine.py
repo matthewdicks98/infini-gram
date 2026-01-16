0e197d21bae82e175aedade466c7c8148fe7c4ce
matthewdicks98 <matthewdicks98@gmail.com>
2026-01-16 15:49:09 +0200

    Testing s3 bucket with auth

--- pkg/infini_gram/py_engine.py
+++ pkg/infini_gram/py_engine.py
@@ -156,7 +156,9 @@ class Engine:
 
     def get_bytes(self, key: str, b: int, e: int) -> bytes:
 
+        t = time.time()
         response = self.s3.get_object(Bucket='infini-gram', Key=key, Range=f'bytes={b}-{e - 1}')
+        print(f"get_bytes - {time.time() - t:.4f}s")
         return response['Body'].read()
 
     def find(self, input_ids: List[int]) -> FindResult:
