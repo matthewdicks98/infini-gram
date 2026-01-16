1dbe365db4382401d95e1ec46290c7afeb5fb3a0
matthewdicks98 <matthewdicks98@gmail.com>
2026-01-16 17:02:02 +0200

    Testing high concurrency

--- pkg/test/s3_test.py
+++ pkg/test/s3_test.py
@@ -18,8 +18,8 @@ def main():
 
     engine = InfiniGramEngine(
         index_dir="",
-        s3_names=['aljazeera'],
-        s3_endpoint_url='https://s3.eu-west-2.wasabisys.com',
+        s3_names=['v4_pileval_llama'],
+        # s3_endpoint_url='https://s3.eu-west-2.wasabisys.com',
         eos_token_id=tokenizer.eos_token_id,
         read_type="s3",
     )
