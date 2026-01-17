f9408dab16802af923d1781e3a58d0d50e1479f6
matthewdicks98 <matthewdicks98@gmail.com>
2026-01-17 13:38:33 +0200

    Vibing block reads

--- pkg/test/s3_test.py
+++ pkg/test/s3_test.py
@@ -26,7 +26,7 @@ def main():
 
     # Timing threading.
     start = time.time()
-    concurrency = 20
+    concurrency = 30
     with ThreadPoolExecutor(max_workers=concurrency) as executor:
         futures = (
             executor.submit(
