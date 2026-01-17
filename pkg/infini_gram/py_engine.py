9dcd6fbfc8ed3216bf63b35a0438f85418840b12
matthewdicks98 <matthewdicks98@gmail.com>
2026-01-17 11:18:21 +0200

    Vibing block reads

--- pkg/infini_gram/py_engine.py
+++ pkg/infini_gram/py_engine.py
@@ -372,7 +372,7 @@ class Engine:
             byte_pos = rank * shard.ptr_size
             # If not in SA cache, fetch new block
             if not (sa_cache["start"] <= byte_pos and byte_pos + shard.ptr_size <= sa_cache["end"]):
-                # print("HIT CACHE get_ptr_at_rank_cached")
+                print("MISS CACHE get_ptr_at_rank_cached")
                 sa_cache["data"], sa_cache["start"], sa_cache["end"] = self.get_bytes_block(
                     shard.sa, byte_pos, shard.tok_cnt * shard.ptr_size, BLOCK_SIZE
                 )
@@ -385,7 +385,7 @@ class Engine:
             nonlocal ds_cache
             # If not in DS cache, fetch new block
             if not (ds_cache["start"] <= ptr and ptr + num_bytes <= ds_cache["end"]):
-                # print("HIT CACHE get_ds_bytes_cached")
+                print("MISS CACHE get_ds_bytes_cached")
                 ds_cache["data"], ds_cache["start"], ds_cache["end"] = self.get_bytes_block(
                     shard.ds, ptr, shard.ds_size, BLOCK_SIZE
                 )
